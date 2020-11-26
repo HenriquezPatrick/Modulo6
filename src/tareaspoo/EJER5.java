182e542d126ede6a9f90779d0e3f804209916716
patrick <patrick@kalihenriquezPC>
2020-11-25 23:10:09 -0600

    version final

---
+++ src/tareaspoo/EJER5.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaspoo;
+
+import java.awt.Component;
+import javax.swing.*;
+/**
+ *
+ * @author colom
+ */
+public class EJER5 { 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero = 2;
+        for(int i = 2; i<100; i++)
+        {
+        if(numero == 0)
+        {
+         JOptionPane.showMessageDialog(null,"LOS NUMERO PARES DEL 2 - 100 ES : "+numero );
+        }
+        }
+        
+    }
+    
+}
